6a0a78e4a1f8ee16ddf99e0bcb76f4d774e95567
fpunjani <35349055+fpunjani@users.noreply.github.com>
2019-11-08 13:06:25 -0700

    profile, search xml added

--- app/src/main/java/com/example/moodtracker/User.java
+++ app/src/main/java/com/example/moodtracker/User.java
@@ -3,10 +3,13 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class User {
 
-    private String userID, email, password;
+    private String id, userID, email, password;
+    private String imageurl;
 
-    public void User(String userID, String email, String password) {
+    public void User(String userID,String imageurl, String id, String email, String password) {
         this.userID = userID;
+        this.id=id;
+        this.imageurl=imageurl;
         this.email = email;
         this.password = password;
     }
@@ -14,6 +17,8 @@ public class User {
     public void User(FirebaseUser firebaseUser) {
         this.email = firebaseUser.getEmail();
         this.password = "";
+        this.imageurl=imageurl;
+        this.id=id;
         this.userID = "";
     }
 
@@ -21,6 +26,15 @@ public class User {
     public String getUserID() { return userID; }
     public String getEmail() { return email; }
     public String getPassword() { return password; }
+    public String getId() { return userID; }
+
+    public String getImageurl() {
+        return imageurl;
+    }
+
+    public void setImageurl(String imageurl) {
+        this.imageurl = imageurl;
+    }
 
     public void setEmail(String email) { this.email = email; }
     public void setPassword(String password) { this.password = password; }
