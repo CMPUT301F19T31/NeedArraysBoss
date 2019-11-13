3585b10616a4eeb4047869ad58ee4bb8b27150f4
RidwanAndalib <andalib369@gmail.com>
2019-11-12 21:09:15 -0700

    navigation

--- app/src/main/java/com/example/moodtracker/SearchFragment.java
+++ app/src/main/java/com/example/moodtracker/SearchFragment.java
@@ -6,15 +6,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.moodtracker.R;
 
-public class search_fragment extends Fragment {
+public class SearchFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
