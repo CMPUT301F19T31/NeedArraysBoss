473e3617393edd6978c8f0e32f15346e529d3e12
ahnafone <ahnafon3@gmail.com>
2019-11-13 14:43:02 -0700

    fixed actionBar error

--- app/src/main/java/com/example/moodtracker/HomeFragment.java
+++ app/src/main/java/com/example/moodtracker/HomeFragment.java
@@ -53,8 +53,6 @@ public class HomeFragment extends Fragment implements AdapterView.OnItemSelected
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        //final TextView textView = root.findViewById(R.id.text_home);
-        //textView.setText(s);
 
         //set up emoji compatibility
         EmojiCompat.Config config = new BundledEmojiCompatConfig(getContext());
