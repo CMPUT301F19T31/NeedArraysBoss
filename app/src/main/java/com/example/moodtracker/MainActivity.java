c2bf778d42429dc9206d06443a26faff9645793d
ahnafone <ahnafon3@gmail.com>
2019-11-24 03:09:14 -0700

    kjnkjn

--- app/src/main/java/com/example/moodtracker/MainActivity.java
+++ app/src/main/java/com/example/moodtracker/MainActivity.java
@@ -27,4 +27,127 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navView, navController);
     }
 
-}
+    private boolean checkMapServices(){
+        if(isServicesOK()){
+            if(isMapsEnabled()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("This application requires GPS to work properly, do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        Intent enableGpsIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivityForResult(enableGpsIntent, PERMISSIONS_REQUEST_ENABLE_GPS);
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    public boolean isMapsEnabled(){
+        final LocationManager manager = (LocationManager) getSystemService( Context.LOCATION_SERVICE );
+
+        if ( !manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
+            buildAlertMessageNoGps();
+            return false;
+        }
+        return true;
+    }
+
+    private void getLocationPermission() {
+
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+            //getChatrooms(); HEREEEEEEEEEEEEEEEEEEEEEEEEEEEE
+            init();
+            //getLastKnownLocation();
+            //getUserDetails();
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    public boolean isServicesOK(){
+        Log.d(TAG, "isServicesOK: checking google services version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+
+        if(available == ConnectionResult.SUCCESS){
+            //everything is fine and the user can make map requests
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //an error occured but we can resolve it
+            Log.d(TAG, "isServicesOK: an error occured but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }else{
+            Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "onActivityResult: called.");
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ENABLE_GPS: {
+                if(mLocationPermissionGranted){
+                    //getChatrooms(); HEREEEEEEEEEEEEEEEEEEE
+                    init();
+                    //getLastKnownLocation();
+                    //getUserDetails();
+                }
+                else{
+                    getLocationPermission();
+                }
+            }
+        }
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(checkMapServices()){
+            if(mLocationPermissionGranted){
+                //getChatrooms(); HEREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+                init();
+                //getLastKnownLocation();
+                //getUserDetails();
+            }
+            else{
+                getLocationPermission();
+            }
+        }
+    }
+*/
--- app/src/main/java/com/example/moodtracker/User.java
+++ app/src/main/java/com/example/moodtracker/User.java
@@ -12,13 +12,38 @@ public class User {
         this.email = email;
         this.password = password;
         this.phone = phone;
-        moodHistory = null;
+    }
+
+    public User(String userID, String email, String password, ArrayList<Mood> moodHistory) {
+        this.userID = userID;
+        this.email = email;
+        this.password = password;
+        this.phone = "";
+        this.moodHistory = moodHistory;
+        imageurl="";
+    }
+
+    public void User(String userID,String imageurl, String id, String email, String password) {
+        this.userID = userID;
+        this.imageurl=imageurl;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
     }
     public User(String userID, String email, String password) {
         this.userID = userID;
         this.email = email;
         this.password = password;
-        this.phone = null;
+        this.phone = "";
+        imageurl="";
+        //moodHistory = null;
+    }
+    public User(String userID, String email, String password, String id, String phone, String imageurl) {
+        this.userID = userID;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.imageurl=imageurl;
         moodHistory = null;
     }
     public User(String userID, String password) {
@@ -33,6 +58,40 @@ public class User {
     }
 
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getImageurl() {
+        return imageurl;
+    }
+
+    public void setImageurl(String imageurl) {
+        this.imageurl = imageurl;
+    }
+
+
+    /*
     //There will be no setter for userID so that it cannot be changed
     public String getUserID() { return userID; }
     public String getEmail() { return email; }
