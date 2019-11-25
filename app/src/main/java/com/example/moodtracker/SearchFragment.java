ea66f359fba958aaef83405314db14845551d501
fpunjani <35349055+fpunjani@users.noreply.github.com>
2019-11-24 17:15:44 -0700

    conflict

--- app/src/main/java/com/example/moodtracker/SearchFragment.java
+++ app/src/main/java/com/example/moodtracker/SearchFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -15,6 +16,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.moodtracker.R;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -23,6 +25,12 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,8 +39,10 @@ public class SearchFragment extends Fragment {
 
     private RecyclerView recyclerView;
     private UserAdapter userAdapter;
-    private List<User> mUsers;
-
+    private ArrayList<User> mUsers;
+    private CollectionReference collectionReference;
+    private FirebaseAuth mAuth;
+    private RecyclerView.LayoutManager usersLM;
 
     EditText search_bar;
 
@@ -43,18 +53,16 @@ public class SearchFragment extends Fragment {
 
         recyclerView = view.findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        mUsers = new ArrayList<>();
+        usersLM = new LinearLayoutManager(getContext());
+        userAdapter = new UserAdapter(getContext(), mUsers);
+        recyclerView.setLayoutManager(usersLM);
+        recyclerView.setAdapter(userAdapter);
 
         search_bar = view.findViewById(R.id.search_bar);
 
-        mUsers = new ArrayList<>();
-        //userAdapter = new UserAdapter(getContext(), mUsers);
-
-        //recyclerView.setAdapter(userAdapter);
-
-
-
-        readUsers();
+        //readUsers();
         search_bar.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -72,19 +80,47 @@ public class SearchFragment extends Fragment {
             }
         });
 
+        //initialize firebase
+        mAuth = FirebaseAuth.getInstance();
+        collectionReference = null;
+
         return view;
     }
 
-    private void searchUsers(String s){
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (mAuth.getCurrentUser() != null) {
+            collectionReference = FirebaseFirestore.getInstance().collection("users");
+            collectionReference.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                @Override
+                public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                    List<DocumentSnapshot> data = queryDocumentSnapshots.getDocuments();
+                    Toast.makeText(getContext(),Integer.toString(data.size()),Toast.LENGTH_SHORT).show();
+                    for (int i = 0; i < data.size(); i++) {
+
+                        mUsers.add(data.get(i).toObject(User.class));
+                        //Toast.makeText(getActivity(), "", Toast.LENGTH_SHORT).show();
+                    }
+                    Toast.makeText(getActivity(), mUsers.get(0).getEmail(), Toast.LENGTH_SHORT).show();
+                    userAdapter.notifyDataSetChanged();
+                }
+            });
+        }
+    }
+
+    private void searchUsers(String s) {
+
+
         Query query = FirebaseDatabase.getInstance().getReference("users").orderByChild("email")
                 .startAt(s)
-                .endAt(s+"\uf8ff");
+                .endAt(s + "\uf8ff");
 
         query.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 mUsers.clear();
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                     User user = snapshot.getValue(User.class);
                     mUsers.add(user);
                 }
@@ -98,31 +134,5 @@ public class SearchFragment extends Fragment {
             }
         });
     }
-
-    private void readUsers() {
-
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
-
-        reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (search_bar.getText().toString().equals("")) {
-                    mUsers.clear();
-                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                        User user = snapshot.getValue(User.class);
-
-                        mUsers.add(user);
-
-                    }
-
-                    userAdapter.notifyDataSetChanged();
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-    }
 }
+
