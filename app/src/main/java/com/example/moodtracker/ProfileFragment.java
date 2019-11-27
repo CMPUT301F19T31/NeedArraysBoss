03701949e42e09dbabd1091f1d9f0058ff92918b
Aisida <aisida@ualberta.ca>
2019-11-26 18:42:51 -0700

    Update ProfileFragment.java

--- app/src/main/java/com/example/moodtracker/ProfileFragment.java
+++ app/src/main/java/com/example/moodtracker/ProfileFragment.java
@@ -1,9 +1,11 @@
 package com.example.moodtracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -27,11 +29,34 @@ public class ProfileFragment extends Fragment {
     User user;
     TextView profile_name;
 
+    FirebaseAuth mAuth;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuth.addAuthStateListener(new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(mAuth.getCurrentUser() == null) {
+                    Intent intent = new Intent(getActivity().getApplicationContext(), Login.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
+            }
+        });
+
+        Button logout = root.findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+            }
+        });
 
         profile_name = root.findViewById(R.id.tv_name);
 
@@ -42,21 +67,16 @@ public class ProfileFragment extends Fragment {
         docRef = FirebaseFirestore.getInstance().collection("users")
                 .document("user"+email);
         docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                    @Override
-                    public void onSuccess(DocumentSnapshot documentSnapshot) {
-                        user = documentSnapshot.toObject(User.class);
-                    }
-                });
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                user = documentSnapshot.toObject(User.class);
+            }
+        });
         String name = user.getEmail();
 
         profile_name.setText(name);
 
-
-
-
         return root;
-
-
     }
-
 }
+
